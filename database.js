a312de1b658bdb59a3fae44be2cec17b3798c8fe
Rafellaa <rafella_angeline@yahoo.com>
2021-04-04 16:33:31 +1000

    pushing

---
+++ database.js
@@ -0,0 +1,15 @@
+// //connecting to DB
+// const { Client } = require('pg');
+
+// //getting the credentials
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+// //connect
+// const database = client.connect();
+
+// module.exports = database;
