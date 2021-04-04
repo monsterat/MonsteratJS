1b392ddc78adf7334cf0952705bd6ba785493dd8
hillarymulyadi <mulyadihillary@gmail.com>
2021-04-04 13:49:37 +1000

    task

---
+++ public/js/task.js
@@ -0,0 +1,45 @@
+var button = document.getElementById("btn");
+var input = document.getElementById("input_newEntry");
+var ul = document.querySelector("ul");
+
+button.addEventListener("click", function() {
+    var li = `<li class="list-group-item">
+    <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+    ${input.value}
+  </li>`
+  $(".list-group").append(li)
+})
+
+
+
+// let tasker = {
+//     selectElements: function() {
+//       this.taskInput = document.getElementById("input-task");
+//       this.taskList = document.getElementById("tasks");
+//       this.taskListChildren = this.taskList.children;
+//       this.addButton = document.getElementById("add-task-btn");
+//       this.errorMessage = document.getElementById("error");
+//     },
+//     buildTask: function(){
+//         let taskListItem, taskCheckbox, taskValue, taskButton, taskTrush;
+//         taskListItem = document.createElement("li");
+//         taskListItem.setAttribute("class", "task");
+//         //checkbox
+//         taskCheckbox = document.createElement("input");
+//         taskCheckbox.setAttribute("type", "checkbox");
+//         //taskvalue
+//         taskValue = document.createTextNode(this.taskInput.Value);
+//         //deletebutton
+//         taskButton =  document.createElement("button");
+        
+//         //append elements to tasklist
+//         taskListItem.appendChild(taskCheckbox);
+//         taskListItem.appendChild(taskValue);
+//         taskListItem.appendChild(taskButton);
+
+//         //add task to tasklist
+//         this.taskList.appendChild(taskListItem);
+        
+//     }
+//   };
+
