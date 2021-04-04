b0cb9bb14eb38b326d6ca51143e67cf3a74e58f0
Rafellaa <rafella_angeline@yahoo.com>
2021-04-05 04:04:06 +1000

    final

--- index.js
+++ index.js
@@ -5,14 +5,6 @@ const expressLayouts = require("express-ejs-layouts");
 const PORT = process.env.PORT || 5000;
 const cool = require('cool-ascii-faces');
 
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('pages/index'))
-//   .get('/cool', (req, res) => res.send(cool()))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- routes/login.js
+++ routes/login.js
@@ -1,28 +1,12 @@
 const express = require("express");
 const router = express.Router();
-// const db = require("../database.js");
 
 router.get("/", (req, res) => {
   res.render("pages/login", {
     title: "Login",
   });
-
-
-//create a user
-// var insertUser;
-// $(enter).on('click', createUser);
-
-
 });
 
-
-
-// function createUser(event) {
-//     event.preventDefault();
-//     //...
-//     thisObject = {course: courseSelect.value, score: scoreText.value, pars: parsText.value, birdies: birdiesText.value};
-//  }
-  
 module.exports = router;
 
 
